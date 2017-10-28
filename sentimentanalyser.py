fc9fc6c724903c78a9e6a276d49522652b77bf45
Joe Farebrother <joseph.farebrother@gmail.com>
2017-10-28 18:35:29 +0100

    company names

--- sentimentanalyser.py
+++ sentimentanalyser.py
@@ -7,15 +7,20 @@ class SentimentAnalyser(object):
         self.neutral_words = webhandler.get_neutral_words()
         self.positive_words = webhandler.get_positive_words()
         self.companies = webhandler.get_company_info()
+        self.company_names = [c.name for c in self.companies]
+        #print(self.companies)
 
     def analyse_tweet(self, tweet):
         """Analyse a tweet, extracting the subject and sentiment"""
         sentiment = 0
+        subject = None
 
         for word in tweet.split(" "):
             if word in self.positive_words:
                 sentiment = sentiment + 1
             if word in self.negative_words:
                 sentiment = sentiment - 1
+            if word in self.company_names:
+                subject = word
 
-        return [(self.companies[0].name, sentiment)]
+        return [(subject or self.companies[0].name, sentiment)]
