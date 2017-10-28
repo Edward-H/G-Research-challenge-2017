5712c2009b21a0ff8d644560edbf5842c6e11d3b
Edward Hart <edward.dan.hart@gmail.com>
2017-10-28 18:54:49 +0100

    Added initial misspelling support.

--- sentimentanalyser.py
+++ sentimentanalyser.py
@@ -1,6 +1,21 @@
 """Analyse tweets"""
 import webhandler
 
+def levenshtein_distance(s1, s2):
+    if len(s1) > len(s2):
+        s1, s2 = s2, s1
+
+    distances = range(len(s1) + 1)
+    for i2, c2 in enumerate(s2):
+        distances_ = [i2+1]
+        for i1, c1 in enumerate(s1):
+            if c1 == c2:
+                distances_.append(distances[i1])
+            else:
+                distances_.append(1 + min((distances[i1], distances[i1 + 1], distances_[-1])))
+        distances = distances_
+    return distances[-1]
+
 class SentimentAnalyser(object):
     def __init__(self):
         self.negative_words = webhandler.get_negative_words()
@@ -25,6 +40,11 @@ class SentimentAnalyser(object):
                 sentiment = sentiment - 1
             if word in self.company_names:
                 subject = word
+            else:
+                for company in self.company_names:
+                    if levenshtein_distance(company, word) <= 3:
+                        subject = company
+                        break
 
         if seen_not:
             sentiment = -sentiment
